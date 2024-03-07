2d4249294f65612400db2cc42433e882d3695b8d
meowmeow <nico55518@gmail.com>
2024-03-07 15:17:47 +0100

    some inequalities

--- someExamples/equalities.lean
+++ someExamples/equalities.lean
@@ -1,4 +1,5 @@
 import Mathlib.Data.Real.Basic
+import Mathlib.Tactic.NormNum
 
 example {a b: ℚ} {h1: a-b = 4} {h2: a*b=1} : (a+b)^2 = 20 :=
 calc
@@ -94,3 +95,42 @@ calc
   _ = (z^2-2)+2 +1 := by ring
   _ = 0 + 2 + 1 := by rw[h1]
   _ = 3 := by ring
+
+example {x y: ℤ} {h1: x+3 ≤ 2} {h2: y+2*x ≥ 3}: y > 3 :=
+calc
+  y = y + 2 * x - 2 *x := by ring
+  _ ≥ 3 - 2 * x := by rel[h2]
+  _ = 9 - 2 * (x+3) := by ring
+  _ ≥ 9 - 2 * 2 := by rel[h1]
+  _ = 5 := by ring
+  _ > 3 := by norm_num
+
+example {r s: ℝ} {h1: s+3 ≥ r} {h2: s+r ≤ 3}: r ≤ 3 :=
+  calc
+    r = (r+r)/ 2 := by ring
+    _ ≤ (r+(s+3)) / 2 := by rel[h1]
+    _ = ((s+r)+3) / 2 := by ring
+    _ ≤ (3+3) / 2 := by rel[h2]
+    _ = 3 := by norm_num
+
+example {x y: ℝ} {h1: y≤x+5} {h2: x≤-2}: x+y ≤ 2 :=
+calc
+  x+y ≤ x+(x+5) := by rel[h1]
+  _ ≤ (-2) + ((-2) + 5) := by rel[h2]
+  _ = 1 := by norm_num
+  _ ≤ 2 := by norm_num
+
+example {u v x y A B: ℝ} {h1: 0<A} {h2: A ≤ 1} {h3: B ≥ 1} {h4: x ≤ B} {h5: y ≤ B} {h6: 0 ≤ u} {h7: u ≤ A} {h8: 0 ≤ v} {h9: v < A} : u*y + v*x + u*v < 3 * A * B :=
+calc
+  u * y + v * x + u * v ≤ u * B + v * B + u * v := by rel[h5, h4]
+    _ ≤ A * B + A * B + A * v := by rel[h7, h9]
+    _ ≤ A * B + A * B + 1 * v := by rel[h2]
+    _ ≤ A * B + A * B + B * v := by rel[h3]
+    _ < A * B + A * B + B * A := by rel[h9]
+    _ = 3 * A * B := by ring
+
+example {t: ℝ} {h1: t ≥ 10} :t^2 - 3*t + 17 ≥ 5 :=
+calc
+  t^2 - 3*t + 17 = t*(t-3)+17 := by ring
+  _ ≥ 10 * (10 - 3) + 17 := by rel[h1]
+  _ ≥ 5 := by norm_num
